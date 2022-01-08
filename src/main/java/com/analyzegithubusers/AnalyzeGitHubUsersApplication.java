101781dddec7feb9b62a0eea7c7f58ac1d672bbf
susannaultralabs <susanna@ultralabs.io>
2022-01-08 14:00:50 +0400

    Initial setup

---
+++ src/main/java/com/analyzegithubusers/AnalyzeGitHubUsersApplication.java
@@ -0,0 +1,13 @@
+package com.analyzegithubusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyzeGitHubUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalyzeGitHubUsersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/analyzegithubusers/AnalyzeGitHubUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.analyzegithubusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyzeGitHubUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
