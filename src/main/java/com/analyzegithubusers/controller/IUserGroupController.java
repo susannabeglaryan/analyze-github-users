27e362e0fb423ee812b8a7e2300697c0cc3a5641
Susanna <37072020+susannabeglaryan@users.noreply.github.com>
2022-01-12 17:47:21 +0400

    Update IUserGroupController.java

--- src/main/java/com/analyzegithubusers/controller/IUserGroupController.java
+++ src/main/java/com/analyzegithubusers/controller/IUserGroupController.java
@@ -5,20 +5,21 @@ import com.analyzegithubusers.controller.dto.UserGroupResponseDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import reactor.core.publisher.Flux;
 
 public interface IUserGroupController {
 
-    @GetMapping("/byCompany")
-    @ApiResponse(content = @Content(mediaType = "application/json"),
+    @GetMapping(path = "/byCompany", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    @ApiResponse(content = @Content(mediaType = "text/event-stream"),
             description = "Returns grouped list of users or empty list if there is no user")
     @Operation(summary = "Get grouped users by company",
             description = "Use this API to retrieve user list grouped by company.")
     Flux<UserGroupResponseDTO> getUsersGroupedByCompany();
 
-    @GetMapping(path = "/byLocation")
-    @ApiResponse(content = @Content(mediaType = "application/json"),
+    @GetMapping(path = "/byLocation", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    @ApiResponse(content = @Content(mediaType = "text/event-stream"),
             description = "Returns grouped list of users or empty list if there is no user")
     @Operation(summary = "Get grouped users by location",
             description = "Use this API to retrieve user list grouped by location.")
